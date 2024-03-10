6eed8d9765ec48cece141edb74e63c9e760757ba
Supprise0901 <141381309+Supprise0901@users.noreply.github.com>
2024-03-10 16:39:26 +0800

    Update URL.java

--- app/src/main/java/com/github/tvbox/osc/bbox/constant/URL.java
+++ app/src/main/java/com/github/tvbox/osc/bbox/constant/URL.java
@@ -2,14 +2,14 @@ package com.github.tvbox.osc.bbox.constant;
 
 public class URL {
     public static String DOMAIN_NAME_PROXY = "https://mirror.ghproxy.com/";
-    public static final String GITHUB_VERSION_PATH = "https://raw.githubusercontent.com/Supprise0901/TVbox/main/version.json";
-    public static final String APK_PATH = "https://raw.githubusercontent.com/Supprise0901/TVbox/Release/app-release.apk";
+    public static final String GITHUB_VERSION_PATH = "https://raw.githubusercontent.com/Supprise0901/TVBox_live/main/version.json";
+    public static final String APK_PATH = "https://raw.githubusercontent.com/Supprise0901/TVBox_live/Release/app-release.apk";
     // 默认线路
     public static final String DEFAULT_API_URL = "https://raw.githubusercontent.com/Supprise0901/Fetch/main/tvbox/影视.json";
     // 默认仓库线路
-    public static final String DEFAULT_STORE_API_URL = "https://raw.githubusercontent.com/Supprise0901/TVBox_warehouse/main/warehouse.txt";
+    public static final String DEFAULT_STORE_API_URL = "https://raw.githubusercontent.com/Supprise0901/TVBox_live/main/warehouse.txt";
     // 默认直播线路
-    public static final String DEFAULT_LIVE_URL = "https://raw.githubusercontent.com/Supprise0901/TVBox_warehouse/main/live.txt";
+    public static final String DEFAULT_LIVE_URL = "https://raw.githubusercontent.com/Supprise0901/TVBox_live/main/live.txt";
 
 
 }
