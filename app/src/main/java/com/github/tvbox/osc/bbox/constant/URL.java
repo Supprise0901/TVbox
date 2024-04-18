943f64c66b56ee2f6bd1ff29460b40065a1cdb1b
supprise <zzsupprise@proton.me>
2024-04-18 22:35:23 +0800

    Update

--- app/src/main/java/com/github/tvbox/osc/bbox/constant/URL.java
+++ app/src/main/java/com/github/tvbox/osc/bbox/constant/URL.java
@@ -1,9 +1,9 @@
 package com.github.tvbox.osc.bbox.constant;
 
 public class URL {
-    public static String DOMAIN_NAME_PROXY = "";
-    public static final String GITHUB_VERSION_PATH = "https://mirror.ghproxy.com/raw.githubusercontent.com/Supprise0901/TVbox/main/version.json";
-    public static final String APK_PATH = "https://mirror.ghproxy.com/raw.githubusercontent.com/Supprise0901/TVbox/main/Release/app_release.apk";
+    public static String DOMAIN_NAME_PROXY = "https://mirror.ghproxy.com/";
+    public static final String GITHUB_VERSION_PATH = "raw.githubusercontent.com/Supprise0901/TVbox/main/version.json";
+    public static final String APK_PATH = "raw.githubusercontent.com/Supprise0901/TVbox/main/Release/app_release.apk";
     // 默认线路
     public static final String DEFAULT_API_URL = "https://voapi.451460.xyz/";
     // 默认仓库线路
